22263855f5e50f27a218d63e7392cf4571fd6050
Harxon <xiansheng929@163.com>
2017-04-11 14:49:02 +0800

    modified:   test_drivers/ex03_lcd/mmap_lcd/mmap_mem.c

--- test_drivers/ex03_lcd/mmap_lcd/mmap_mem.c
+++ test_drivers/ex03_lcd/mmap_lcd/mmap_mem.c
@@ -9,35 +9,62 @@
 
 #define Debug() printf("(%s):%d\n", __func__, __LINE__); 
 
+//define MMAP_SIZE sysconf(_SC_PAGESIZE)
+#define MMAP_SIZE 4096UL
+#define MMAP_MASK (MMAP_SIZE - 1)
 
 int main(int argc, const char *argv[])
 {
-	int fd_mem = -1;
-	unsigned long gpx2con_phy_addr = 0;
-	unsigned long* vaddr_base = NULL;
-	unsigned long* gpx2con_vaddr = NULL;
-	unsigned long* gpx2dat_vaddr = NULL;
+	int fd = -1, i=0;
+	void* map_addr = NULL;
+	void* vir_addr = NULL;
+	void* dat_addr = NULL;
+	unsigned long target[2] = {0x11000c40, 0x11000c44};
+	char type[3] = "2_7";
+	unsigned long opt[2] ={1<<(type[2]*4), 1<<(type[2])};
 
-	gpx2con_phy_addr = strtoul("0x11000c44", 0, 0);
-
-	fd_mem = open("/dev/mem", O_RDWR | O_SYNC);
-	{
-		Debug();
-		vaddr_base = mmap(0, 4096UL, PROT_READ|PROT_WRITE,  MAP_SHARED, fd_mem, gpx2con_phy_addr & ~(4096UL - 1));
-		gpx2con_vaddr = vaddr_base + (gpx2con_phy_addr & (4096UL -1));
-		*gpx2con_vaddr = (1<<7);
-		printf("%lp:%#x\n", gpx2con_vaddr, *gpx2con_vaddr);
 #if 0
-		gpx2dat_vaddr = gpx2con_vaddr + 1;
-		printf("%p:%#x\n", gpx2dat_vaddr, *gpx2dat_vaddr);
-		*gpx2dat_vaddr = (0<<7); 
-		sleep(1);
-		*gpx2dat_vaddr = (1<<7); 
-#endif
+	switch(argc){
+	case 4:{
+			   opt[0] = strtoul(argv[3], 0, 0);
+		   }
+	case 3:{
+			   type[2] = tolower(argv[2][2]);
+		   }
+	case 2:{
+			   target[0] = strtoul(argv[1], 0, 0);
+		   }
+	case 1:{
+			   break;
+		   }
+	default:break;
 	}
-	Debug();
-	munmap(gpx2con_vaddr, 8);
+#endif
 
-	getchar();
+	fd = open("/dev/mem", O_RDWR | O_SYNC);
+	if(fd == -1){
+		Debug();
+		exit(-1);
+	}
+
+	map_addr = mmap(NULL, MMAP_SIZE, PROT_READ|PROT_WRITE,  MAP_SHARED, fd, target[0] & ~MMAP_MASK);
+	vir_addr = map_addr + (target[0] & MMAP_MASK);
+	dat_addr = vir_addr + 4;
+
+	printf("gpx2con_vaddr:%lp, data:%#x\n", vir_addr, *(unsigned long*)vir_addr);
+	printf("gpx2dat_vaddr:%lp, data:%#x\n", dat_addr, *(unsigned long*)dat_addr);
+	*(unsigned long*)vir_addr &= ~(0xf<<28);
+	*(unsigned long*)vir_addr |= (1<<28);
+	for(i=0; i<2; i++){
+		*(unsigned long*)dat_addr |= (1<<7); 
+		sleep(1);
+		*(unsigned long*)dat_addr &= ~(1<<7); 
+		sleep(1);
+	}
+	printf("gpx2con_vaddr:%lp, data:%#x\n", vir_addr, *(unsigned long*)vir_addr);
+	printf("gpx2dat_vaddr:%lp, data:%#x\n", dat_addr, *(unsigned long*)dat_addr);
+
+	Debug();
+	munmap(vir_addr, MMAP_SIZE);
 	return 0;
 }
